76a76dd7be56ef08b6cf2b0ca7302fa4b3cc9f5c
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-06-16 15:05:56 -0300

    Backup

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,14 +1,31 @@
 ﻿namespace EspacioEmpleado;
 
-public class EspacioEmpleado
+public class Empleado
 {
     private string nombre;
     private string apellido;
     private DateTime fechaDeNacimiento;
-    private char estadoCivil;
+    private EstadoCivil estadoCivil;
     private DateTime fechaIngresoAEmpresa;
     private double sueldoBasico;
     private Cargo cargo;
 
-    
+    public Empleado(string nombre, string apellido, string fechaDeNacimiento, EstadoCivil estadoCivil, string fechaIngresoAEmpresa, double sueldoBasico, Cargo cargo)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaDeNacimiento = DateTime.Parse(fechaDeNacimiento, System.Globalization.CultureInfo.InvariantCulture);
+        this.estadoCivil = estadoCivil;
+        this.fechaIngresoAEmpresa = DateTime.Parse(fechaIngresoAEmpresa, System.Globalization.CultureInfo.InvariantCulture);
+        this.sueldoBasico = sueldoBasico;
+        this.cargo = cargo;
+    }
+
+    public int Antiguedad()
+    {
+        DateTime Antiguedad = DateTime.Parse(this.fechaIngresoAEmpresa - DateTime.Today);
+
+        
+        return 0;
+    }
 }
---
+++ SisPersonal/EstadoCivil.cs
@@ -0,0 +1,11 @@
+//Considero que es necesario crear un enum para el estado civil
+
+namespace EspacioEmpleado;
+
+public enum EstadoCivil
+{
+    S,
+    C,
+    V,
+    D
+}
